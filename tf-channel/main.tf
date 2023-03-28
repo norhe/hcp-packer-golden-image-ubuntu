3dc58077aee051a6581e0479bf2860e01c21dbe9
Ehron <ehron@nepal.lan>
2023-03-28 14:01:25 -0400

    update tf to deploy image from channel

--- tf-channel/main.tf
+++ tf-channel/main.tf
@@ -5,12 +5,12 @@ provider "aws" {
 }
 
 data "hcp_packer_iteration" "ubuntu" {
-  bucket_name = "learn-packer-ubuntu"
-  channel     = "production"
+  bucket_name = var.bucket_name
+  channel     = var.channel
 }
 
 data "hcp_packer_image" "ubuntu_us_east_2" {
-  bucket_name    = "learn-packer-ubuntu"
+  bucket_name    = var.bucket_name
   cloud_provider = "aws"
   iteration_id   = data.hcp_packer_iteration.ubuntu.ulid
   region         = "us-east-2"
@@ -20,6 +20,6 @@ resource "aws_instance" "app_server" {
   ami           = data.hcp_packer_image.ubuntu_us_east_2.cloud_image_id
   instance_type = "t2.micro"
   tags = {
-    Name = "Learn-HCP-Packer"
+    Name = "Approved 22.04 Image"
   }
 }
