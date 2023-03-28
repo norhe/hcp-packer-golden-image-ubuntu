6db338eec685f1b05c5647d4310ddc55121bd2c8
Ehron <ehron@nepal.lan>
2023-03-28 14:10:24 -0400

    fix variable

--- tf-channel/variables.tf
+++ tf-channel/variables.tf
@@ -12,7 +12,7 @@ variable "channel" {
   type = string
   description = "HCP Packer Image Channel"
   validation {
-    condition = contains(["Production", "UAT", "latest"], var.environment)
+    condition = contains(["Production", "UAT", "latest"], var.channel)
     error_message = "Valid value is one of the following: Production, UAT, latest."
   }
 }
